9a7674ae1e09d60f348437b7eca559ec4a6ce107
[Muhammad Jamil Raza Attari] <jamilraza001@gmail.com>
2024-03-01 18:29:57 +0500

    first commit

---
+++ Assiments03/index.js
@@ -0,0 +1,5 @@
+// storing name and quote in variable
+var quote = "Injustice anywhere is a threat to justice everywhere.";
+var author = "Martin Luther King";
+// printing quote and author name 
+console.log("".concat(author, " once said,\"").concat(quote, "\""));
---
+++ Assiments03/index.ts
@@ -0,0 +1,9 @@
+// storing name and quote in variable
+
+let quote:string = "Injustice anywhere is a threat to justice everywhere.";
+
+let author:string = "Martin Luther King";
+
+// printing quote and author name 
+
+console.log(`${author} once said,"${quote}"`);
