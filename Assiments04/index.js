071918ecf94ef72fdfaf439193214a9be63583eb
[Muhammad Jamil Raza Attari] <jamilraza001@gmail.com>
2024-03-02 03:01:38 +0500

    first commit

---
+++ Assiments04/index.js
@@ -0,0 +1,8 @@
+// storing a person name in variable 
+var famous_person = "Martin Luther King";
+// storing a message in variable
+var message = "Injustice anywhere is a threat to justice everywhere.";
+// printing message
+console.log(message);
+// in case you want to print both message and famous_person name
+console.log(famous_person + " " + message);
---
+++ Assiments04/index.ts
@@ -0,0 +1,16 @@
+// storing a person name in variable 
+
+let famous_person:string = "Martin Luther King";
+
+// storing a message in variable
+
+let message:string = "Injustice anywhere is a threat to justice everywhere."
+
+// printing message
+
+console.log(message);
+
+
+// in case you want to print both message and famous_person name
+
+// console.log(famous_person + " " +message );
