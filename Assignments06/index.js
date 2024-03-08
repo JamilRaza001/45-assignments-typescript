367625d242ab55e30857f5f887490c3759bcaf13
[Muhammad Jamil Raza Attari] <jamilraza001@gmail.com>
2024-03-08 12:03:17 +0500

    first commit

---
+++ Assignments06/index.js
@@ -0,0 +1,13 @@
+// Addition
+let Addition = 5 + 3;
+console.log("The sum of 5 + 3 =", Addition);
+// Subscription
+let Subscription = 10 - 2;
+console.log("The Subscription of 10 - 2 =", Subscription);
+// Multiplication
+let Multiplication = 4 * 2;
+console.log("The Multiplication of 4 * 2 =", Multiplication);
+// Division
+let Division = 16 / 2;
+console.log("The Division of 4 * 2 =", Division);
+export {};
---
+++ Assignments06/index.ts
@@ -0,0 +1,27 @@
+// Addition
+
+let Addition:number = 5 + 3;
+
+console.log("The sum of 5 + 3 =",Addition);
+
+// Subscription
+
+let Subscription:number = 10 - 2;
+
+console.log("The Subscription of 10 - 2 =",Subscription);
+
+
+
+// Multiplication
+
+let Multiplication:number = 4 * 2;
+
+console.log("The Multiplication of 4 * 2 =",Multiplication);
+
+
+// Division
+
+let Division:number = 16 / 2;
+
+console.log("The Division of 4 * 2 =",Division);
+
