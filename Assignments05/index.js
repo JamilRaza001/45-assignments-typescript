cc034c09faeb4878a2594030973cba3a12bfbd92
[Muhammad Jamil Raza Attari] <jamilraza001@gmail.com>
2024-03-08 11:45:33 +0500

    first commit

---
+++ Assignments05/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// Storing person name with white space in variable
+let PersonName = "\t \n  Jamil Raza \t\n ";
+console.log(PersonName);
+// Storing person name with out white space in variable
+PersonName = PersonName.trim();
+console.log(PersonName);
---
+++ Assignments05/index.ts
@@ -0,0 +1,10 @@
+// Storing person name with white space in variable
+
+let PersonName:string = "\t \n  Jamil Raza \t\n ";
+console.log(PersonName);
+
+// Storing person name with out white space in variable
+
+ PersonName = PersonName.trim();
+console.log(PersonName);
+
